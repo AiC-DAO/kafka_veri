2a2f82f40d6d1b4e89718d933d46c5418b8b3f71
laole <laole1210264337@yahoo.com>
2023-11-17 15:43:42 +0800

    恢复消费者测试代码

--- main.go
+++ main.go
@@ -27,77 +27,57 @@ func main() {
 		}
 	}()
 
-	// log.Printf("初始化消费者====\n")
-	// consumer, err := sarama.NewConsumer([]string{BROKER_ADDR}, config)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// 	log.Fatalln("222222")
-	// }
-	// defer func() {
-	// 	if err := consumer.Close(); err != nil {
-	// 		log.Fatalln(err)
-	// 	}
-	// }()
+	log.Printf("初始化消费者====\n")
+	consumer, err := sarama.NewConsumer([]string{BROKER_ADDR}, config)
+	if err != nil {
+		log.Fatalln(err)
+		log.Fatalln("222222")
+	}
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
 
-	// partitionConsumer, err := consumer.ConsumePartition("to-ender", 0, sarama.OffsetNewest)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// 	log.Fatalln("333333")
-	// }
-	// defer func() {
-	// 	if err := partitionConsumer.Close(); err != nil {
-	// 		log.Fatalln(err)
-	// 	}
-	// }()
+	partitionConsumer, err := consumer.ConsumePartition("to-ender", 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Fatalln(err)
+		log.Fatalln("333333")
+	}
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
 
 	log.Printf("初始化完成====\n")
 
-	// go func() {
-	// 	for {
-	// 		log.Printf("开始发送消息====\n")
+	go func() {
+		for {
+			log.Printf("开始发送消息====\n")
 
-	// 		message := &sarama.ProducerMessage{
-	// 			Topic: "to-ender",
-	// 			Value: sarama.StringEncoder("Hello, World!"),
-	// 		}
+			message := &sarama.ProducerMessage{
+				Topic: "to-ender",
+				Value: sarama.StringEncoder("Hello, World!"),
+			}
 
-	// 		partition, offset, err := producer.SendMessage(message)
-	// 		if err != nil {
-	// 			log.Printf("Failed to send message: %v\n", err)
-	// 		} else {
-	// 			fmt.Printf("Message sent to partition %d at offset %d\n", partition, offset)
-	// 		}
+			partition, offset, err := producer.SendMessage(message)
+			if err != nil {
+				log.Printf("Failed to send message: %v\n", err)
+			} else {
+				fmt.Printf("Message sent to partition %d at offset %d\n", partition, offset)
+			}
 
-	// 		log.Printf("消息发送完毕====\n")
+			log.Printf("消息发送完毕====\n")
 
-	// 		time.Sleep(1 * time.Second)
-	// 	}
-	// }()
-
-	for {
-		log.Printf("开始发送消息====\n")
-
-		message := &sarama.ProducerMessage{
-			Topic: "to-ender",
-			Value: sarama.StringEncoder("Hello, World!"),
+			time.Sleep(1 * time.Second)
 		}
-
-		partition, offset, err := producer.SendMessage(message)
-		if err != nil {
-			log.Printf("Failed to send message: %v\n", err)
-		} else {
-			fmt.Printf("Message sent to partition %d at offset %d\n", partition, offset)
-		}
-
-		log.Printf("消息发送完毕====\n")
-
-		time.Sleep(1 * time.Second)
-	}
+	}()
 
 	// 消费者获取消息并打印
-	// for msg := range partitionConsumer.Messages() {
-	// 	log.Printf("消费者接收消息====\n")
-	// 	fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
-	// 	fmt.Printf("Message value:%s\n", string(msg.Value))
-	// }
+	for msg := range partitionConsumer.Messages() {
+		log.Printf("消费者接收消息====\n")
+		fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+		fmt.Printf("Message value:%s\n", string(msg.Value))
+	}
 }
